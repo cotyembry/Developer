0f341474c0576f3c7fef25b061cf40577f58323e
cotyembry <cotyembry@gmail.com>
2017-06-22 18:06:49 -0500

    Update logonCommands.cmd

--- logonCommands/logonCommands.cmd
+++ logonCommands/logonCommands.cmd
@@ -1,4 +1,4 @@
-#C:\Windows\System32\cmd.exe /K C:\Users\johnembry\Developer\logonCommands.cmd
+#C:\Windows\System32\cmd.exe /K C:\Users\johnembry\Developer\logonCommands\logonCommands.cmd
 DOSKEY ls=dir
 DOSKEY c=cls
 DOSKEY clear=cls
